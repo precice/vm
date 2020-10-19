53bad286c6682d184833de84bcc5be324191ea84
Gerasimos Chourdakis <makishourdakis@gmail.com>
2020-10-19 14:55:55 +0200

    Improve install-paraview.sh

--- preCICE-Ubuntu-20.04/install-paraview.sh
+++ preCICE-Ubuntu-20.04/install-paraview.sh
@@ -3,8 +3,10 @@ set -ex
 
 USER="vagrant"
 
-curl "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.8&type=binary&os=Linux&downloadFile=ParaView-5.8.1-MPI-Linux-Python3.7-64bit.tar.gz" > /home/vagrant/paraview.tar.gz
-cd /home/vagrant/
-sudo -u ${USER} tar -xzf paraview.tar.gz
-rm paraview.tar.gz
-sudo -u ${USER} ln -sf /home/vagrant/ParaView-5.8.1-MPI-Linux-Python3.7-64bit/bin/paraview /home/vagrant/Desktop/
+if [ ! -d "paraview" ]; then
+    mkdir paraview
+    PARAVIEW_URL="https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.8&type=binary&os=Linux&downloadFile=ParaView-5.8.1-MPI-Linux-Python3.7-64bit.tar.gz"
+    wget --no-check-certificate --quiet -O - ${PARAVIEW_URL} | tar -xz -C paraview
+    ln -sf /home/vagrant/paraview/ParaView-5.8.1-MPI-Linux-Python3.7-64bit/bin/paraview /home/vagrant/Desktop/
+    chown vagrant:vagrant /home/vagrant/Desktop/paraview
+fi

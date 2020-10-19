f2dbd80cc5314fc61460b0e9c667fac6ae1ca6c9
Gerasimos Chourdakis <makishourdakis@gmail.com>
2020-10-19 15:30:59 +0200

    Add a shortcut to the keyboard settings on the Desktop

--- preCICE-Ubuntu-20.04/install-basics.sh
+++ preCICE-Ubuntu-20.04/install-basics.sh
@@ -15,12 +15,16 @@ sudo apt-get install -y thunar xfce4-terminal terminator bash-completion tree ev
 # Install the VirtualBox guest additions
 sudo apt-get install -y virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
 
-# Create /home/vagrant/desktop
-sudo -u ${USER} mkdir -p /home/vagrant/Desktop
-chown vagrant:vagrant Desktop
+# Create /home/${USER}/desktop
+sudo -u ${USER} mkdir -p /home/${USER}/Desktop
+chown ${USER}:${USER} /home/${USER}/Desktop
 
 # Use US-English keyboard layout
 L='us' && sudo sed -i 's/XKBLAYOUT=\"\w*"/XKBLAYOUT=\"'$L'\"/g' /etc/default/keyboard
+# Add a shortcut to the keyboard options on the Desktop
+cp /usr/share/applications/xfce-keyboard-settings.desktop /home/${USER}/Desktop/
+chmod +x /home/${USER}/Desktop/xfce-keyboard-settings.desktop
+chown ${USER}:${USER} /home/${USER}/Desktop/xfce-keyboard-settings.desktop
 
 # Set a hostname
 echo "precicevm" | sudo tee /etc/hostname

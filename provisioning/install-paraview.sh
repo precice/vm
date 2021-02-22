89f908575e6a23bc36bb705ee941195d71cc3f1c
Gerasimos Chourdakis <makishourdakis@gmail.com>
2021-02-22 20:30:28 +0100

    Add ParaView to PATH
    Closes #2

--- provisioning/install-paraview.sh
+++ provisioning/install-paraview.sh
@@ -7,6 +7,8 @@ if [ ! -d "paraview" ]; then
     mkdir paraview
     PARAVIEW_URL="https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.8&type=binary&os=Linux&downloadFile=ParaView-5.8.1-MPI-Linux-Python3.7-64bit.tar.gz"
     wget --no-check-certificate --quiet -O - ${PARAVIEW_URL} | tar -xz -C paraview
-    ln -sf /home/vagrant/paraview/ParaView-5.8.1-MPI-Linux-Python3.7-64bit/bin/paraview /home/vagrant/Desktop/
-    chown vagrant:vagrant /home/vagrant/Desktop/paraview
+    ln -sf /home/${USER}/paraview/ParaView-5.8.1-MPI-Linux-Python3.7-64bit/bin/paraview /home/${USER}/Desktop/
+    chown ${USER}:${USER} /home/${USER}/Desktop/paraview
+    # Add ParaView to PATH
+    echo "export PATH=\"/home/${USER}/paraview/ParaView-5.8.1-MPI-Linux-Python3.7-64bit/bin:\${PATH}\"" >> /home/${USER}/.bashrc
 fi

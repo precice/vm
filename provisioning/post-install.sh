b4986ceed13ad3e5e99aee1e399a9e6bd389498f
Gerasimos Chourdakis <makishourdakis@gmail.com>
2021-04-28 21:13:15 +0200

    Disable screensaver and screen lock (#21)

--- provisioning/post-install.sh
+++ provisioning/post-install.sh
@@ -3,3 +3,9 @@ set -ex
 
 # Create a link to the default shared folder
 ln -sf /vagrant/ ~/Desktop/shared
+
+# Disable the screensaver and automatic screen lock
+{
+    echo "xset s off -dpms"
+    echo "gsettings set org.gnome.desktop.screensaver lock-enabled false"
+} >> ~/.bashrc

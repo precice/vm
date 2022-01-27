5fc0c849d5c5c9183f2689871e2532490f378e0b
Frédéric Simonis <simonisfrederic@gmail.com>
2022-01-27 13:35:36 +0100

    Disable tests in preCICE build

--- provisioning/install-precice.sh
+++ provisioning/install-precice.sh
@@ -14,7 +14,7 @@ fi
     cd precice
     git pull
     mkdir -p build && cd build/
-    cmake -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug -Wno-dev ..
+    cmake -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=OFF -Wno-dev ..
     make -j 2
     rm -fv ./*.deb && make package
     sudo apt-get install -y ./libprecice2_*.deb

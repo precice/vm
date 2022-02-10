a13252fcfb8cb12c0ad1b422c8d5aa28d2aecd99
Gerasimos Chourdakis <makishourdakis@gmail.com>
2022-02-10 17:30:51 +0100

    Fix dune

--- provisioning/install-dune.sh
+++ provisioning/install-dune.sh
@@ -1,35 +1,72 @@
 #!/usr/bin/env bash
 set -ex
 
-# Install DUNE
-wget --quiet https://www.dune-project.org/download/2.8.0/dune-common-2.8.0.tar.gz
-tar xvzf dune-common-2.8.0.tar.gz
-rm -fv dune-common-2.8.0.tar.gz
+# Make a folder to collect all DUNE-related code (-p to allow re-provisioning)
+mkdir -p dune && cd dune
+
+# Get required DUNE modules
+if [ ! -d "dune-common/" ]; then
+    git clone --branch v2.8.0 --depth=1 https://gitlab.dune-project.org/core/dune-common.git
+fi
+
+if [ ! -d "dune-istl/" ]; then
+    git clone --branch v2.8.0 --depth=1 https://gitlab.dune-project.org/core/dune-istl.git
+fi
+
+if [ ! -d "dune-localfunctions/" ]; then
+    git clone --branch v2.8.0 --depth=1 https://gitlab.dune-project.org/core/dune-localfunctions.git
+fi
+
+if [ ! -d "dune-grid/" ]; then
+    git clone --branch v2.8.0 --depth=1 https://gitlab.dune-project.org/core/dune-grid.git
+fi
+
+if [ ! -d "dune-geometry/" ]; then
+    git clone --branch v2.8.0 --depth=1 https://gitlab.dune-project.org/core/dune-geometry.git
+fi
+
+if [ ! -d "dune-functions/" ]; then
+    git clone --branch v2.8.0 --depth=1 https://gitlab.dune-project.org/staging/dune-functions.git
+fi
+
+if [ ! -d "dune-uggrid/" ]; then
+    git clone --branch v2.8.0 --depth=1 https://gitlab.dune-project.org/staging/dune-uggrid.git
+fi
+
+if [ ! -d "dune-typetree/" ]; then
+    git clone --branch v2.8.0 --depth=1 https://gitlab.dune-project.org/staging/dune-typetree.git
+fi
+
+if [ ! -d "dune-foamgrid/" ]; then
+    git clone --branch releases/2.8 --depth=1 https://gitlab.dune-project.org/extensions/dune-foamgrid.git
+fi
+
+# Get the dune-elastodynamics module (solid solver)
+if [ ! -d "dune-elastodynamics/" ]; then
+    git clone --branch master --depth=1 https://github.com/maxfirmbach/dune-elastodynamics.git
+fi
 (
-    cd dune-common-2.8.0
-    ./bin/dunecontrol all
+    cd dune-elastodynamics
+    git pull
 )
-
-# Add DUNE to PATH and apply.
-# We first export to a separate script, so that we can load it here (non-interactive shell).
-{
-    echo "export PATH=\"\${HOME}/dune-common-2.8.0/bin:\${PATH}\""
-} >> ~/.dune-bashrc
-
-echo ". \${HOME}/.dune-bashrc" >> ~/.bashrc
-# shellcheck source=/dev/null
-. ~/.dune-bashrc
 
 # Get the DUNE-preCICE adapter
 if [ ! -d "dune-adapter/" ]; then
-    git clone --depth=1 --branch main --depth=1 https://github.com/precice/dune-adapter.git
+    git clone --branch main --depth=1 https://github.com/precice/dune-adapter.git
 fi
 (
     cd dune-adapter/dune-precice
     git pull
-    # Build the DUNE-preCICE adapter
-    ../../dune-common-2.8.0/bin/dunecontrol --current all
 )
 
-# Add the DUNE adapter to PATH
-echo "export PATH=\"\${HOME}/dune-adapter/dune-precice/bin:\${PATH}\"" >> ~/.bashrc
+# Build all the DUNE and DUNE-preCICE related modules
+DUNE_CONTROL_PATH=~/dune ./dune-common/bin/dunecontrol all
+
+# Set the DUNE_CONTROL_PATH (DUNE recursively finds modules in this directory)
+echo "export DUNE_CONTROL_PATH=\"\${HOME}/dune\"" >> ~/.bashrc
+
+# Copy the built example code to the tutorials
+cp ~/dune/dune-adapter/dune-precice-howto/build-cmake/examples/dune-perpendicular-flap ~/tutorials/perpendicular-flap/solid-dune
+
+# We are done with DUNE, let's do back home
+cd ~

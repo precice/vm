93972d5a3b560b4b04165b5093a54d3db82e70e5
Gerasimos Chourdakis <makishourdakis@gmail.com>
2022-02-11 00:28:18 +0100

    Add terminator on the desktop
    closes #15

--- provisioning/post-install.sh
+++ provisioning/post-install.sh
@@ -4,6 +4,10 @@ set -ex
 # Create a link to the default shared folder
 ln -sf /vagrant/ ~/Desktop/shared
 
+# Add terminator on the desktop
+cp /usr/share/applications/terminator.desktop ~/Desktop/
+chmod +x ~/Desktop/terminator.desktop
+
 # Disable the screensaver and automatic screen lock
 {
     echo "xset s off -dpms"

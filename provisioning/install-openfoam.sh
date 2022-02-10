1fcf9ded4107e5b2776556b65c69b49d3b59bc76
Gerasimos Chourdakis <makishourdakis@gmail.com>
2022-02-10 23:57:31 +0100

    Replace groovyBC by prebuilt packages, to save time and space

--- provisioning/install-openfoam.sh
+++ provisioning/install-openfoam.sh
@@ -20,8 +20,19 @@ fi
 )
 
 # Get swak4Foam (provides groovyBC, needed for the turek-hron-fsi3 tutorial)
-sudo apt-get install -y mercurial
-hg clone http://hg.code.sf.net/p/openfoam-extend/swak4Foam swak4Foam
-cd swak4Foam
-hg checkout develop
-openfoam2112 ./AllwmakeAll
+#
+# # Option 1: Build from source
+# sudo apt-get install -y mercurial
+# hg clone http://hg.code.sf.net/p/openfoam-extend/swak4Foam swak4Foam
+# (
+#     cd swak4Foam
+#     hg checkout develop
+#     openfoam2112 ./AllwmakeAll
+# )
+#
+# # Remove some swak4Foam files to save space (approx. 150MB)
+# rm -rfv .~swak4Foam
+# sudo apt-get purge --autoremove -y mercurial # This also removes Python2, yipieh!
+#
+# # Option 2: Use pre-built binaries
+# # (see Vagrantfile, rebuild and update for OpenFOAM version other than v2112)

b008094769cd79fb4fbbebfe863a4fd980616b92
Gerasimos Chourdakis <makishourdakis@gmail.com>
2022-09-19 10:26:48 +0200

    Rename preCICE branch to main

--- provisioning/install-precice.sh
+++ provisioning/install-precice.sh
@@ -5,10 +5,10 @@ set -ex
 sudo apt-get install -y cmake libeigen3-dev libxml2-dev libboost-all-dev petsc-dev python3-dev python3-numpy
 
 # Get preCICE from GitHub:
-# - Always get the latest master, no need for versioning
+# - Always get the latest main, no need for versioning
 # - Build in Debug mode, so that users can report bugs
 if [ ! -d "precice/" ]; then
-    git clone --depth=1 --branch master https://github.com/precice/precice.git
+    git clone --depth=1 --branch main https://github.com/precice/precice.git
 fi
 (
     cd precice

5fbe5d3529fe79543f5dbaddca810670d59dc299
Gerasimos Chourdakis <makishourdakis@gmail.com>
2024-04-16 16:42:44 +0200

    Update provisioning/install-config-visualizer.sh
    Co-authored-by: Frédéric Simonis <simonisfrederic@gmail.com>

--- provisioning/install-config-visualizer.sh
+++ provisioning/install-config-visualizer.sh
@@ -7,7 +7,8 @@ sudo apt-get install -y graphviz
 sudo apt-get install -y libcairo2-dev libgirepository1.0-dev gcc libcairo2-dev pkg-config gir1.2-gtk-4.0
 
 # Get the config-visualizer from PIP
-pipx install precice-config-visualizer precice-config-visualizer-gui
+pipx install precice-config-visualizer
+pipx install precice-config-visualizer-gui
 
 # Add the config-visualizer to PATH
 echo "export PATH=\"\${HOME}/config-visualizer/bin:\${PATH}\"" >>~/.bashrc

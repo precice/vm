b4921db5dc20f7f93878c1a15178a19f77d8f4d4
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-04-16 16:57:28 +0200

    Added the GUI .desktop file

--- provisioning/install-config-visualizer.sh
+++ provisioning/install-config-visualizer.sh
@@ -12,3 +12,6 @@ pipx install precice-config-visualizer-gui
 
 # Add the config-visualizer to PATH
 echo "export PATH=\"\${HOME}/config-visualizer/bin:\${PATH}\"" >>~/.bashrc
+
+# Add the GUI in the apps menu
+mkdir -p ~/.local/share/applications && wget -O ~/.local/share/applications/org.precice.configvisualizer.desktop https://gist.githubusercontent.com/fsimonis/a08c3771abf808b0534d658bcb563f90/raw/10717f5b2afbf2d3c05ecb9f5c7eeaae4bbe868a/org.precice.configvisualizer.desktop

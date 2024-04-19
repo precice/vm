a714000df1758c592a8ce8b37c931059d6b10863
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-04-19 12:34:01 +0200

    Fix pipx venv path for config-visualizer

--- provisioning/install-config-visualizer.sh
+++ provisioning/install-config-visualizer.sh
@@ -7,8 +7,10 @@ sudo apt-get install -y graphviz
 sudo apt-get install -y build-essential pkg-config python3-dev libcairo2-dev libgirepository1.0-dev gir1.2-gtk-3.0
 
 # Get the config-visualizer from PIP
-pipx install --force precice-config-visualizer==1.1.3
-pipx install --force precice-config-visualizer-gui==0.1.0
+PRECEICE_CONFIG_VISUALIZER_VERSION=1.1.3
+PRECEICE_CONFIG_VISUALIZER_GUI_VERSION=0.1.0
+pipx install --force precice-config-visualizer==${PRECEICE_CONFIG_VISUALIZER_VERSION}
+pipx install --force precice-config-visualizer-gui==${PRECEICE_CONFIG_VISUALIZER_GUI_VERSION}
 
 # Add the config-visualizer to PATH
 # shellcheck disable=SC2016
@@ -17,6 +19,6 @@ echo 'export PATH="${HOME}/.local/bin:${PATH}"' >> ~/.bashrc
 # Add the GUI in the apps menu
 mkdir -p ~/.local/share/applications ~/.local/share/icons
 
-CV_LOC_SHARE=~/.local/pipx/venvs/precice-config-visualizer-gui/share
+CV_LOC_SHARE=~/.local/pipx/venvs/precice-config-visualizer-gui\=\=${PRECEICE_CONFIG_VISUALIZER_GUI_VERSION}/share
 cp $CV_LOC_SHARE/applications/org.precice.config_visualizer.desktop ~/.local/share/applications/
 cp $CV_LOC_SHARE/icons/hicolor/scalable/apps/org.precice.config_visualizer.svg ~/.local/share/icons/

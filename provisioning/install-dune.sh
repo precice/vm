b306848138af172b0074c02fa74fbfa30063ecc7
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-04-23 10:18:54 +0200

    Build DUNE first, DuMuX next

--- provisioning/install-dune.sh
+++ provisioning/install-dune.sh
@@ -2,15 +2,11 @@
 set -ex
 
 # Make a folder to collect all DUNE-related code
-# The installdumux.py script assumes a dumux/ directory, and we
-# reuse the same installation, to reduce space and confusion.
 mkdir -p dune-dumux && cd dune-dumux
 
-# Modules used both by DUNE and DuMuX
+# Modules used by DUNE and DuMuX
 # While the installdumux.py script clones these as well,
 # we explicitly clone them to control the versions.
-# For example, the dune-grid (release/2.9) version cloned by installdumux.py
-# does not work with DUNE
 if [ ! -d "dune-common/" ]; then
     git clone --branch v2.9.1 --depth=1 https://gitlab.dune-project.org/core/dune-common.git
 fi
@@ -39,21 +35,6 @@ if [ ! -d "dune-typetree/" ]; then
     git clone --depth 1 --branch v2.9.1 https://gitlab.dune-project.org/staging/dune-typetree.git
 fi
 
-if [ ! -d "dumux/" ]; then
-    git clone --depth 1 --branch releases/3.8 https://git.iws.uni-stuttgart.de/dumux-repositories/dumux.git
-fi
-
-./dune-common/bin/dunecontrol --opts=dumux/cmake.opts all
-
-# Get the DuMuX-preCICE adapter
-if [ ! -d "dumux-adapter" ]; then
-    git clone  --depth 1 --branch v2.0.0 https://github.com/precice/dumux-adapter.git
-fi
-
-# Build the DuMuX-preCICE adapter
-./dune-common/bin/dunecontrol --only=dumux-precice all
-
-# Get additional DUNE modules required for the plain DUNE adapter
 if [ ! -d "dune-foamgrid/" ]; then
     # The missing v in the tag in this module originates from the project itself
     git clone --depth 1 --branch 2.9.1 https://gitlab.dune-project.org/extensions/dune-foamgrid.git
@@ -66,9 +47,6 @@ fi
 if [ ! -d "dune-uggrid/" ]; then
     git clone --depth 1 --branch v2.9.1 https://gitlab.dune-project.org/staging/dune-uggrid.git
 fi
-
-# Build all the additional DUNE modules
-DUNE_CONTROL_PATH=~/dune-dumux ./dune-common/bin/dunecontrol all
 
 # Get the dune-elastodynamics module (solid solver for the plain dune adapter)
 if [ ! -d "dune-elastodynamics/" ]; then
@@ -88,8 +66,19 @@ fi
     git pull
 )
 
-# Build all the DUNE and DUNE-preCICE related modules
-DUNE_CONTROL_PATH=~/dune-dumux ./dune-common/bin/dunecontrol all
+./dune-common/bin/dunecontrol all
+
+# Get DuMuX and the DuMuX-preCICE adapter
+if [ ! -d "dumux/" ]; then
+    git clone --depth 1 --branch releases/3.8 https://git.iws.uni-stuttgart.de/dumux-repositories/dumux.git
+fi
+
+if [ ! -d "dumux-adapter/" ]; then
+    git clone  --depth 1 --branch v2.0.0 https://github.com/precice/dumux-adapter.git
+fi
+
+# Build the DuMuX-preCICE adapter
+./dune-common/bin/dunecontrol --opts=dumux/cmake.opts --module=dumux-precice all
 
 # Set the DUNE_CONTROL_PATH (DUNE recursively finds modules in this directory)
 echo "export DUNE_CONTROL_PATH=\"\${HOME}/dune-dumux\"" >> ~/.bashrc

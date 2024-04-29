5e2c8fb0f5b6f7c7945904c561af550afc664dd2
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-04-29 11:51:48 +0200

    Install fmiprecice and micro-manager from pipx instead of setting up venv

--- provisioning/install-fmiprecice.sh
+++ provisioning/install-fmiprecice.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -ex
 
-python -m venv ~/python-venvs/fmiprecice
-# shellcheck disable=SC1090 # We don't need to lint this external script
-source ~/python-venvs/fmiprecice/bin/activate
-
-# Install the FMI runner from PIP
-python -m pip install fmiprecice
-
-deactivate
+# Install the FMI runner (stand-alone application) from PyPI
+pipx install fmiprecice
--- provisioning/install-micro-manager.sh
+++ provisioning/install-micro-manager.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-python -m venv ~/python-venvs/micro-manager
-# shellcheck disable=SC1090 # We don't need to lint this external script
-source ~/python-venvs/micro-manager/bin/activate
+# Install the micro-manager (stand-alone application) from PyPI
+pipx install micro-manager-precice
 
-python -m pip install micro-manager-precice
-
-deactivate

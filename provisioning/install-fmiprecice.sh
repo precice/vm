71e11a11bc4bc6d5a0a1f00c46eaec4df4ba599b
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-04-29 11:15:10 +0200

    Fix shellcheck directives

--- provisioning/install-fmiprecice.sh
+++ provisioning/install-fmiprecice.sh
@@ -2,7 +2,7 @@
 set -ex
 
 python -m venv ~/python-venvs/fmiprecice
-# shellcheck disable=SC1090 We don't need to lint this external script
+# shellcheck disable=SC1090 # We don't need to lint this external script
 source ~/python-venvs/fmiprecice/bin/activate
 
 # Install the FMI runner from PIP
--- provisioning/install-micro-manager.sh
+++ provisioning/install-micro-manager.sh
@@ -2,7 +2,7 @@
 set -ex
 
 python -m venv ~/python-venvs/micro-manager
-# shellcheck disable=SC1090 We don't need to lint this external script
+# shellcheck disable=SC1090 # We don't need to lint this external script
 source ~/python-venvs/micro-manager/bin/activate
 
 python -m pip install micro-manager-precice

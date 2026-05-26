86ece682bc032afa49862632ca71755a5527effc
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-05-26 14:03:01 +0200

    Add FreeCAD to training-fsi, rename venv

--- provisioning/install-training-fsi.sh
+++ provisioning/install-training-fsi.sh
@@ -2,11 +2,17 @@
 set -ex
 
 # Additional packages for the FSI training module
-python -m venv ~/python-venvs/openfoam-training
+python -m venv ~/python-venvs/training-fsi
 # shellcheck disable=SC1090 # We don't need to lint this external script
-source ~/python-venvs/openfoam-training/bin/activate
+source ~/python-venvs/training-fsi/bin/activate
 python -m pip install pyfoam
-pipx install ccx2paraview
 deactivate
+pipx install ccx2paraview
 
-# FreeCAD is missing, to save space. Get it from https://www.freecad.org/downloads.php
+# FreeCAD AppImage from https://www.freecad.org/downloads.php (~780MB)
+(
+  cd ~/Desktop
+  wget https://github.com/FreeCAD/FreeCAD/releases/download/1.1.1/FreeCAD_1.1.1-Linux-x86_64-py311.AppImage
+  mv FreeCAD_1.1.1-Linux-x86_64-py311.AppImage FreeCAD.AppImage
+  chmod +x FreeCAD.AppImage
+)

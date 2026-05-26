32115bac3f4ec6c2097de6807feb1d41ea40c8fb
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-05-26 13:39:07 +0200

    Fix pyfoam package name

--- provisioning/install-training-fsi.sh
+++ provisioning/install-training-fsi.sh
@@ -5,7 +5,7 @@ set -ex
 python -m venv ~/python-venvs/openfoam-training
 # shellcheck disable=SC1090 # We don't need to lint this external script
 source ~/python-venvs/openfoam-training/bin/activate
-python -m pip install openfoam-training
+python -m pip install pyfoam
 pipx install ccx2paraview
 deactivate
 

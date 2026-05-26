24c993cbf48274d8f503fab0561ff0225c205ad5
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-05-26 16:07:53 +0200

    Switch Julia installation to pipx

--- provisioning/install-julia-bindings.sh
+++ provisioning/install-julia-bindings.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-python -m venv ~/python-venvs/julia
-# shellcheck disable=SC1090 # We don't need to lint this external script
-source ~/python-venvs/julia/bin/activate
-
 # install latest julia
-python -m pip install jill
+pipx install jill
 jill install --confirm
 
 # install preCICE bindings
@@ -13,5 +9,3 @@ julia -e 'using Pkg; Pkg.add("PreCICE")'
 
 # to test the installation, run the following command:
 # julia -e 'using Pkg; Pkg.test("PreCICE")'
-
-deactivate

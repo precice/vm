b6704e8cb9dcee437c35b43e9d61d1ddc56fce1d
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-05-27 07:47:00 +0200

    Remove --user option from pip install

--- provisioning/install-training-python.sh
+++ provisioning/install-training-python.sh
@@ -3,6 +3,6 @@ set -ex
 
 # shellcheck disable=SC1090 # We don't need to lint this external script
 source ~/python-venvs/pyprecice/bin/activate
-pip3 install --user matplotlib pandas polars
-pip3 install --user nutils
+pip3 install matplotlib pandas polars
+pip3 install nutils
 deactivate

5ca6ed44de8f7353c0cae5a40c457a1ab25e9853
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-06-02 12:19:01 +0200

    Update comments in install-su2.sh

--- provisioning/install-su2.sh
+++ provisioning/install-su2.sh
@@ -8,7 +8,7 @@ source ~/python-venvs/su2precice/bin/activate
 python -m pip install mpi4py setuptools # pyprecice is installed by the tutorials
 sudo apt-get -y install swig
 
-# Get SU2 7.5.1 from GitHub
+# Get SU2 from GitHub
 git clone --depth=1 --branch=v7.5.1 --recurse-submodules https://github.com/su2code/SU2.git
 
 # Add SU2 and the SU2 adapter to PATH and apply.
@@ -46,18 +46,7 @@ fi
     # See https://github.com/ninja-build/ninja/commit/9cf13cd1ecb7ae649394f4133d121a01e191560b
     sed -i 's/pipes/shlex/g' externals/ninja/configure.py
 
-    # Disable CGNS with -Denable-cgns=false. Workaround for:
-    # default: FAILED: externals/cgns/libcgns.a.p/adf_ADF_internals.c.o
-    # default: cc -Iexternals/cgns/libcgns.a.p -Iexternals/cgns -I../externals/cgns -Iexternals/cgns/hdf5 -I../externals/cgns/hdf5 -I/usr/lib/x86_64-linux-gnu/openmpi/include -I/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi -fdiagnostics-color=always -D_FILE_OFFSET_BITS=64 -std=c99 -O3 -fPIC -Wno-unused-result -Wno-unused-parameter -Wno-unused-variable -Wno-unused-but-set-variable -Wno-sign-compare -Wno-error=unused-function -Wno-pedantic -Wno-error=stringop-truncation -Wno-stringop-truncation -Wno-error=absolute-value -Wno-error=class-memaccess -MD -MQ externals/cgns/libcgns.a.p/adf_ADF_internals.c.o -MF externals/cgns/libcgns.a.p/adf_ADF_internals.c.o.d -o externals/cgns/libcgns.a.p/adf_ADF_internals.c.o -c ../externals/cgns/adf/ADF_internals.c
-    # default: ../externals/cgns/adf/ADF_internals.c: In function ‘ADFI_open_file’:
-    # default: ../externals/cgns/adf/ADF_internals.c:194:17: error: implicit declaration of function ‘fileno’ [-Wimplicit-function-declaration]
-    # default:   194 | # define FILENO fileno
-    # default:       |                 ^~~~~~
-    # default: ../externals/cgns/adf/ADF_internals.c:5531:32: note: in expansion of macro ‘FILENO’
-    # default:  5531 |    f_ret = ftmp == NULL ? -1 : FILENO(ftmp);
-    # default:       |                                ^~~~~~
-    #
-    # Also set -Denable-tecio=false, as it is not needed, to save space.
+    # Disable unnecessary dependencies to save space and to workaround an issue that CGNS does not build.
     ./meson.py build -Denable-pywrapper=true -Denable-cgns=false -Denable-tecio=false --prefix="${SU2_RUN}" &&\
     ./ninja -C build install
 )
